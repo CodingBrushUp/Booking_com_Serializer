a49670726e6fdcb1956be12b5db78a0cda4a8973
Ali <alen_zx@yahoo.com>
2021-11-19 08:40:43 +0300

    - Result folder changed to c:\resources - Null Node Element Exception Handled - Check Write Permission for Result File Stream

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 using Task1.Models;
 
-string fileName = "Hotel.json";
+string fileName = "c:\\Resources\\Hotel.json";
 string docUrl = $"c:\\Resources\\task 1 - Kempinski Hotel Bristol Berlin, Germany - Booking.com.html";
 
 var doc = new HtmlDocument();
@@ -21,7 +21,6 @@ Hotel GetHotelInfo(HtmlDocument doc)
     hotel.Classification = getInnerHtmlByClass(doc, "//span[contains(@class,'js--hp-scorecard-scoreword')]");
     hotel.Address = getInnerHtmlById(doc, "hp_address_subtitle");
     hotel.Description = getInnerHtmlById(doc, "summary");
-    
 
     hotel.RoomCategories = doc.DocumentNode.SelectNodes("//td[@class='ftd']").Select(x => x.InnerHtml.Trim()).ToList();
     hotel.AlternativeHotels = doc.DocumentNode.SelectNodes("//a[@class='althotel_link']").Select(x => x.GetAttributeValue("href", String.Empty).Trim()).ToList();
@@ -29,16 +28,19 @@ Hotel GetHotelInfo(HtmlDocument doc)
 }
 string getInnerHtmlById(HtmlDocument doc, string Id)
 {
-    return doc.GetElementbyId(Id).InnerHtml.Trim();
+        return doc?.GetElementbyId(Id)?.InnerHtml?.Trim() ?? String.Empty;
 }
 string getInnerHtmlByClass(HtmlDocument doc, string className)
 {
-    return doc.DocumentNode.SelectNodes(className).FirstOrDefault().InnerHtml.Trim();
+    return doc?.DocumentNode?.SelectNodes(className)?.FirstOrDefault()?.InnerHtml?.Trim() ?? String.Empty;
 }
 async Task SaveToJsonFile(Hotel hotel, string fileName)
 {
-    using FileStream createStream = File.Create(fileName);
-    var options = new JsonSerializerOptions { WriteIndented = true };
-    await JsonSerializer.SerializeAsync(createStream, hotel, options: options);
-    await createStream.DisposeAsync();
+    using FileStream createStream = new(fileName, FileMode.Create);
+    if (createStream.CanWrite)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        await JsonSerializer.SerializeAsync(createStream, hotel, options: options);
+        await createStream.DisposeAsync();
+    }
 }
